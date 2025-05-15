e27e3de3112cfd6dfda44f20f0822051a7c17df6
VicenteJFV <vifarias304@gmail.com>
2025-05-15 19:43:53 -0400

    CRUD usuarios

---
+++ service/src/main/java/com/perfulandia/service/repository/UsuarioRepository.java
@@ -0,0 +1,9 @@
+package com.perfulandia.service.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.perfulandia.service.model.Usuario;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+}
